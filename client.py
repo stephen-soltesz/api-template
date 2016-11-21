109b7a05c17c1f6322c3a07c83448dff33fb6a34
Stephen Soltesz <stephen.soltesz@gmail.com>
2016-11-20 19:16:43 -0500

    add flags for api hostname and version

--- client.py
+++ client.py
@@ -2,7 +2,9 @@
 
 
 import argparse
+import logging
 import pprint
+import os
 import sys
 
 from apiclient import discovery
@@ -16,6 +18,17 @@ USER_AGENT = 'sample-cmdline-tool/1.0'
 OAUTH_DISPLAY_NAME = 'Sample API Commandline Tool'
 CLIENT_ID = '294885104230-a8i6fnv7pcgsg0chm0r8vtihcftkcurj.apps.googleusercontent.com'
 CLIENT_SECRET = 'pK_9Txg8jRP9ll_ttYxwxlzk'
+CLIENT_ID = '294885104230-ue8r3k8on02m0kjsu8vh5ulnke7imnrf.apps.googleusercontent.com'
+CLIENT_SECRET = 'g1bLl_oz-2bGqI12Blvr3tLU'
+
+CREDENTIALS_FILE = os.path.join(os.environ['HOME'], '.api_credentials')
+
+OAUTH2_FLOW = oauth2client.client.OAuth2WebServerFlow(
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    scope=SCOPE,
+    user_agent=USER_AGENT,
+    oauth_displayname=OAUTH_DISPLAY_NAME, prompt='consent')
 
 def main(argv):
   # Parse command line flags used by the oauth2client library.
@@ -23,37 +36,46 @@ def main(argv):
       description='Auth sample',
       formatter_class=argparse.RawDescriptionHelpFormatter,
       parents=[tools.argparser])
+  parser.add_argument('--hostname', default='dash-test-1.appspot.com',
+      help='FQDN for the appengine app.')
+  parser.add_argument('--api', default='greeting', help='The API name.')
+  parser.add_argument('--version', default='v1', help='The API version.')
+
   flags = parser.parse_args(argv[1:])
 
   # Acquire and store oauth token.
-  storage = oauth2client.file.Storage('guestbook.dat')
+  storage = oauth2client.file.Storage(CREDENTIALS_FILE)
   credentials = storage.get()
 
   if credentials is None or credentials.invalid:
-    flow = oauth2client.client.OAuth2WebServerFlow(
-        client_id=CLIENT_ID,
-        client_secret=CLIENT_SECRET,
-        scope=SCOPE,
-        user_agent=USER_AGENT,
-        oauth_displayname=OAUTH_DISPLAY_NAME)
-    credentials = tools.run_flow(flow, storage, flags)
-  http = httplib2.Http()
-  http = credentials.authorize(http)
+    credentials = tools.run_flow(OAUTH2_FLOW, storage, flags)
+  http = credentials.authorize(httplib2.Http())
   credentials.refresh(http)
 
   # Build a service object for interacting with the API.
-  # api_root = 'http://192.168.0.116:8080/_ah/api'
-  api_root = 'https://dash-test-1.appspot.com/_ah/api'
-  api = 'greeting'
-  version = 'v1'
-  discovery_url = '%s/discovery/v1/apis/%s/%s/rest' % (api_root, api, version)
-  print discovery_url
+  # NOTE: the default discovery url points to googleapis.com, so we must
+  # construct one that points to the appengine app api.
+  discovery_url = 'https://{0}/_ah/api/discovery/v1/apis/{1}/{2}/rest'.format(
+      flags.hostname, flags.api, flags.version)
+  logging.info(discovery_url)
+
   service = discovery.build(
-      api, version, discoveryServiceUrl=discovery_url,
+      flags.api, flags.version, discoveryServiceUrl=discovery_url,
       http=http, cache_discovery=False)
 
   response = service.authcheck().execute()
   pprint.pprint(response)
+
+  response = service.create(body={
+      'id':'clientcheck', 'message':'this is another test'}).execute()
+  pprint.pprint(response)
+
+  response = service.list().execute()
+  pprint.pprint(response)
+
+  response = service.delete(body={}, id='clientcheck').execute()
+  pprint.pprint(response)
+
   response = service.list().execute()
   pprint.pprint(response)
 
