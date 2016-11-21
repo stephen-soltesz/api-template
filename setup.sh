b8634ed111d6199003fbadcb03df025c87450177
Stephen Soltesz <stephen.soltesz@gmail.com>
2016-11-20 19:17:28 -0500

    use curl to download

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget 'https://github.com/GoogleCloudPlatform/endpoints-proto-datastore/blob/zipfile-branch/endpoints_proto_datastore.zip?raw=true'
+curl -O 'https://github.com/GoogleCloudPlatform/endpoints-proto-datastore/blob/zipfile-branch/endpoints_proto_datastore.zip?raw=true'
 unzip endpoints_proto_datastore.zip
