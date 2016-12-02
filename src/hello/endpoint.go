2defe6beac129879765916ca6ec48f295c0ded37
Stephen Soltesz <soltesz@google.com>
2016-12-02 18:19:44 -0500

    add get, delete, get, functions

--- src/hello/endpoint.go
+++ src/hello/endpoint.go
@@ -24,7 +24,15 @@ type AddRequest struct {
 
 // Add creates a new host based on the fields in AddRequest, stores it in the
 // datastore, and returns it.
-func (api *HostsAPI) Add(c context.Context, r *AddRequest) (*HostRecord, error) {
+func (api *HostsAPI) Create(c context.Context, r *AddRequest) (*HostRecord, error) {
+	// u, err := endpoints.CurrentBearerTokenUser(c, []string{endpoints.EmailScope}, []string{endpoints.APIExplorerClientID})
+	// if err != nil {
+	// 	log.Errorf(c, "Failed to get user for add request: %s", err)
+	// 	return nil, err
+	// }
+	// log.Infof(c, "User: %s", u.ID)
+	// log.Infof(c, "User: %s", u.Email)
+	// log.Infof(c, "User: %s", u.ClientID)
 
 	h, err := PutHost(c, r.IPAddress)
 	if err != nil {
@@ -37,6 +45,15 @@ func (api *HostsAPI) Add(c context.Context, r *AddRequest) (*HostRecord, error) 
 
 // List returns a list of all the existing quotes.
 func (api *HostsAPI) List(c context.Context) (*Hosts, error) {
+	// u, err := endpoints.CurrentBearerTokenUser(c, []string{endpoints.EmailScope}, []string{endpoints.APIExplorerClientID})
+	// if err != nil {
+	// 	log.Errorf(c, "Failed to get user for add request: %s", err)
+	// 	return nil, err
+	// }
+	// log.Infof(c, "User: %s", u.ID)
+	// log.Infof(c, "User: %s", u.Email)
+	// log.Infof(c, "User: %s", u.ClientID)
+
 	hosts := []HostRecord{}
 
 	_, err := datastore.NewQuery("hostrecord").GetAll(c, &hosts)
@@ -49,4 +66,45 @@ func (api *HostsAPI) List(c context.Context) (*Hosts, error) {
 	// 	quotes[i].UID = k
 	// }
 	return &Hosts{hosts}, nil
+}
+
+type DeleteRequest struct {
+	IPAddress string `json:"id"`
+}
+
+func (api *HostsAPI) Delete(c context.Context, r *DeleteRequest) (*HostRecord, error) {
+	key := datastore.NewKey(c, "hostrecord", r.IPAddress, 0, nil)
+	err := datastore.Delete(c, key)
+	return nil, err
+}
+
+func (api *HostsAPI) Setup(c context.Context) (*HostRecord, error) {
+	h, err := PutHost(c, "127.0.0.1")
+	h, err = PutHost(c, "192.168.1.100")
+	h, err = PutHost(c, "10.128.3.7")
+	return h, err
+}
+
+type GetRequest struct {
+	IPAddress string `json:"id"`
+}
+
+// defined with "hosts/{id}" path template
+func (api *HostsAPI) Get(c context.Context, r *GetRequest) (*HostRecord, error) {
+	// u, err := endpoints.CurrentBearerTokenUser(c, []string{endpoints.EmailScope}, []string{endpoints.APIExplorerClientID})
+	//err := nil
+	// if false { // err != nil {
+	// log.Errorf(c, "Failed to get user for add request: %s", err)
+	// } else {
+	// log.Infof(c, "User: %s", u.ID)
+	// log.Infof(c, "User: %s", u.Email)
+	// log.Infof(c, "User: %s", u.ClientID)
+	// }
+	log.Infof(c, "Request: %#v", r)
+	h, err := GetHost(c, r.IPAddress)
+	if err != nil {
+		log.Errorf(c, "Failed to get Host %s: %s", r.IPAddress, err)
+		return nil, err
+	}
+	return h, nil
 }
