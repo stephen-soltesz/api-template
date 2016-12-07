1975f938fa9c1891fdef25c489f23d8d5d39c096
Stephen Soltesz <soltesz@google.com>
2016-12-06 19:06:45 -0500

    Add AuthDecorator for checking user authentication.

--- src/hello/endpoint.go
+++ src/hello/endpoint.go
@@ -1,13 +1,18 @@
 package hello
 
 import (
+	"fmt"
+	"strings"
+
+	"github.com/GoogleCloudPlatform/go-endpoints/endpoints"
 	"golang.org/x/net/context"
 	"google.golang.org/appengine/datastore"
 	"google.golang.org/appengine/log"
 )
 
+var PsClientID = "294885104230-ue8r3k8on02m0kjsu8vh5ulnke7imnrf.apps.googleusercontent.com"
+
 // A HostsAPI struct defines all the endpoints of the hosts API.
-// It will have functions for CRUD like Add, List etc.
 type HostsAPI struct {
 }
 
@@ -17,43 +22,67 @@ type Hosts struct {
 	Hosts []HostRecord `json:"hosts"`
 }
 
-// AddRequest contains all the fields needed to create a new Host.
-type AddRequest struct {
-	IPAddress string
+// CreateRequest contains all the fields needed to create a new Host record.
+type CreateRequest struct {
+	IPAddress string `json:"id"`
 }
 
-// Add creates a new host based on the fields in AddRequest, stores it in the
-// datastore, and returns it.
-func (api *HostsAPI) Create(c context.Context, r *AddRequest) (*HostRecord, error) {
-	// u, err := endpoints.CurrentBearerTokenUser(c, []string{endpoints.EmailScope}, []string{endpoints.APIExplorerClientID})
-	// if err != nil {
-	// 	log.Errorf(c, "Failed to get user for add request: %s", err)
-	// 	return nil, err
-	// }
-	// log.Infof(c, "User: %s", u.ID)
-	// log.Infof(c, "User: %s", u.Email)
-	// log.Infof(c, "User: %s", u.ClientID)
+// GetRequest contains fields for fetching a Host record.
+type GetRequest struct {
+	IPAddress string `json:"id"`
+}
 
+// DeleteRequest contains fields for deleting a Host record.
+type DeleteRequest struct {
+	IPAddress string `json:"id"`
+}
+
+// Used to authenticate all endpoints requests.
+func AuthDecorator(c context.Context) (context.Context, error) {
+	// Get the original request to inspect the requested path.
+	r := endpoints.HTTPRequest(c)
+	if r == nil {
+		msg := fmt.Sprintf("Failed to get request from context.")
+		log.Errorf(c, msg)
+		return nil, fmt.Errorf(msg)
+	}
+	if strings.HasPrefix(r.URL.Path, "/_ah/spi/") {
+		// Allow "backend" requests to discover the endpoints configuration without authentication.
+		return c, nil
+	}
+
+	// For all other paths, enforce the client ids.
+	u, err := endpoints.CurrentBearerTokenUser(c, []string{endpoints.EmailScope}, []string{endpoints.APIExplorerClientID, PsClientID})
+	if err != nil {
+		log.Errorf(c, "Failed to get user for add request: %s", err)
+		return nil, err
+	}
+	// TODO: Make this generic, e.g. with flags or storing admin users in Datastore.
+	if u.Email == "soltesz@google.com" || u.Email == "stephen.soltesz@gmail.com" {
+		msg := fmt.Sprintf("User is not authorized: %s", u.Email)
+		log.Errorf(c, msg)
+		return nil, fmt.Errorf(msg)
+	}
+	return c, nil
+}
+
+///////////////////////////////////////////////////////////////////
+// Each HostsAPI method.
+///////////////////////////////////////////////////////////////////
+
+// Add creates a new host based on the fields in CreateRequest, stores it in the
+// datastore, and returns it.
+func (auth *HostsAPI) Create(c context.Context, r *CreateRequest) (*HostRecord, error) {
 	h, err := PutHost(c, r.IPAddress)
 	if err != nil {
 		log.Errorf(c, "Failed to put %s in datastore: %s", r.IPAddress, err)
 		return nil, err
 	}
-
 	return h, nil
 }
 
 // List returns a list of all the existing quotes.
-func (api *HostsAPI) List(c context.Context) (*Hosts, error) {
-	// u, err := endpoints.CurrentBearerTokenUser(c, []string{endpoints.EmailScope}, []string{endpoints.APIExplorerClientID})
-	// if err != nil {
-	// 	log.Errorf(c, "Failed to get user for add request: %s", err)
-	// 	return nil, err
-	// }
-	// log.Infof(c, "User: %s", u.ID)
-	// log.Infof(c, "User: %s", u.Email)
-	// log.Infof(c, "User: %s", u.ClientID)
-
+func (auth *HostsAPI) List(c context.Context) (*Hosts, error) {
 	hosts := []HostRecord{}
 
 	_, err := datastore.NewQuery("hostrecord").GetAll(c, &hosts)
@@ -61,45 +90,18 @@ func (api *HostsAPI) List(c context.Context) (*Hosts, error) {
 		log.Errorf(c, "Failed to query hostrecords: %s", err)
 		return nil, err
 	}
-
-	// for i, k := range keys {
-	// 	quotes[i].UID = k
-	// }
 	return &Hosts{hosts}, nil
 }
 
-type DeleteRequest struct {
-	IPAddress string `json:"id"`
-}
-
-func (api *HostsAPI) Delete(c context.Context, r *DeleteRequest) (*HostRecord, error) {
+// Deletes a Host record.
+func (auth *HostsAPI) Delete(c context.Context, r *DeleteRequest) (*HostRecord, error) {
 	key := datastore.NewKey(c, "hostrecord", r.IPAddress, 0, nil)
 	err := datastore.Delete(c, key)
 	return nil, err
 }
 
-func (api *HostsAPI) Setup(c context.Context) (*HostRecord, error) {
-	h, err := PutHost(c, "127.0.0.1")
-	h, err = PutHost(c, "192.168.1.100")
-	h, err = PutHost(c, "10.128.3.7")
-	return h, err
-}
-
-type GetRequest struct {
-	IPAddress string `json:"id"`
-}
-
-// defined with "hosts/{id}" path template
-func (api *HostsAPI) Get(c context.Context, r *GetRequest) (*HostRecord, error) {
-	// u, err := endpoints.CurrentBearerTokenUser(c, []string{endpoints.EmailScope}, []string{endpoints.APIExplorerClientID})
-	//err := nil
-	// if false { // err != nil {
-	// log.Errorf(c, "Failed to get user for add request: %s", err)
-	// } else {
-	// log.Infof(c, "User: %s", u.ID)
-	// log.Infof(c, "User: %s", u.Email)
-	// log.Infof(c, "User: %s", u.ClientID)
-	// }
+// Gets a Host record from the Datastore.
+func (auth *HostsAPI) Get(c context.Context, r *GetRequest) (*HostRecord, error) {
 	log.Infof(c, "Request: %#v", r)
 	h, err := GetHost(c, r.IPAddress)
 	if err != nil {
@@ -107,4 +109,13 @@ func (api *HostsAPI) Get(c context.Context, r *GetRequest) (*HostRecord, error) 
 		return nil, err
 	}
 	return h, nil
+}
+
+// Setup the datastore with temporary data for testing.
+// TODO(soltesz): remove this function.
+func (api *HostsAPI) Setup(c context.Context) (*HostRecord, error) {
+	h, err := PutHost(c, "127.0.0.1")
+	h, err = PutHost(c, "192.168.1.100")
+	h, err = PutHost(c, "10.128.3.7")
+	return h, err
 }
