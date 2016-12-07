dba37b126926aed61e334da38edc789d366e7eff
Stephen Soltesz <soltesz@google.com>
2016-12-07 10:55:27 -0500

    move files

---
+++ python/setup_python.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -O 'https://github.com/GoogleCloudPlatform/endpoints-proto-datastore/blob/zipfile-branch/endpoints_proto_datastore.zip?raw=true'
+unzip endpoints_proto_datastore.zip
--- setup.sh
+++ setup.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-curl -O 'https://github.com/GoogleCloudPlatform/endpoints-proto-datastore/blob/zipfile-branch/endpoints_proto_datastore.zip?raw=true'
-unzip endpoints_proto_datastore.zip
+FILE=google-cloud-sdk-135.0.0-linux-x86_64.tar.gz
+if [[ ! -f $FILE ]] ; then
+    wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/${FILE}
+fi
+
+if [[ ! -d /usr/local/google-cloud-sdk ]] ; then
+    sudo tar -C /usr/local -xvf "${FILE}"
+fi
+
+sudo /usr/local/google-cloud-sdk/install.sh
+
+export PATH=$PATH:/usr/local/google-cloud-sdk/bin/
+echo "export $PATH"
+echo "gcloud init"
