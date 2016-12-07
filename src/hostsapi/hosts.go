513b206a95f913ad83385f6802a66f7663ddd59a
Stephen Soltesz <soltesz@google.com>
2016-12-07 12:18:35 -0500

    Rename Host record type.

--- src/hostsapi/hosts.go
+++ src/hostsapi/hosts.go
@@ -1,3 +1,16 @@
+// Copyright 2016 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package hello
 
 import (
@@ -7,17 +20,15 @@ import (
 
 	"github.com/GoogleCloudPlatform/go-endpoints/endpoints"
 
-	// --logs_path=your-path/your-logfile-name
-
-	// "google.golang.org/appengine"
 	"golang.org/x/net/context"
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/datastore"
 	"google.golang.org/appengine/log"
 )
 
-type HostRecord struct {
-	IPAddress string    `json:"ip_address" endpoints:"req" datastore:"id"`
+// Host records represent a single machine.
+type Host struct {
+	IPAddress string    `json:"ip_address" endpoints:"req"`
 	Created   time.Time `json:"created" datastore:"created"`
 }
 
@@ -28,7 +39,7 @@ func init() {
 		panic(err)
 	}
 
-	// adapt the name, method, and path for each method.
+	// Setup the endpoint for each API function.
 	info := api.MethodByName("List").Info()
 	info.Name, info.HTTPMethod, info.Path = "list", "GET", "hosts"
 
@@ -52,22 +63,22 @@ func init() {
 	http.HandleFunc("/", HelloHandler)
 }
 
-func GetHost(ctx context.Context, ipaddr string) (*HostRecord, error) {
-	var h HostRecord
-	key := datastore.NewKey(ctx, "hostrecord", ipaddr, 0, nil)
+func GetHost(ctx context.Context, ipaddr string) (*Host, error) {
+	var h Host
+	key := datastore.NewKey(ctx, "HostsGo", ipaddr, 0, nil)
 	if err := datastore.Get(ctx, key, &h); err != nil {
 		return nil, err
 	}
 	return &h, nil
 }
 
-func PutHost(ctx context.Context, ipaddr string) (*HostRecord, error) {
-	h := HostRecord{
+func PutHost(ctx context.Context, ipaddr string) (*Host, error) {
+	h := Host{
 		IPAddress: ipaddr,
 		// NOTE: python fails to decode the format of the 'date-time' type at nanosecond resolution.
 		Created: time.Now().UTC().Truncate(time.Second),
 	}
-	key := datastore.NewKey(ctx, "hostrecord", ipaddr, 0, nil)
+	key := datastore.NewKey(ctx, "HostsGo", ipaddr, 0, nil)
 	if _, err := datastore.Put(ctx, key, &h); err != nil {
 		return nil, err
 	}
@@ -107,9 +118,9 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		log.Infof(ctx, "Saved the HostRecord for %#v", h)
+		log.Infof(ctx, "Saved the Host record for %#v", h)
 	}
-	log.Infof(ctx, "Retrieved the HostRecord for %#v", h)
+	log.Infof(ctx, "Retrieved the Host record for %#v", h)
 
 	// Basic response.
 	w.Header().Set("Content-Type", "application/json")
