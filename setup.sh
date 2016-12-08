99fce1efc82f4d40bf1fde4d23a9e9ebb1da9bd4
Stephen Soltesz <soltesz@google.com>
2016-12-07 20:35:03 -0500

    Use $HOME/local as installation for sdk

--- setup.sh
+++ setup.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-FILE=google-cloud-sdk-135.0.0-linux-x86_64.tar.gz
-if [[ ! -f $FILE ]] ; then
-    wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/${FILE}
+set -ex
+
+SDK=google-cloud-sdk-135.0.0-linux-x86_64.tar.gz
+if [[ ! -f ${SDK} ]] ; then
+    wget "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/${SDK}"
 fi
 
-if [[ ! -d /usr/local/google-cloud-sdk ]] ; then
-    sudo tar -C /usr/local -xvf "${FILE}"
+if [[ ! -d ${HOME}/local/google-cloud-sdk ]] ; then
+    mkdir -p ${HOME}/local/google-cloud-sdk
+    tar -C ${HOME}/local -xvf "${SDK}"
 fi
 
-sudo /usr/local/google-cloud-sdk/install.sh
+${HOME}/local/google-cloud-sdk/install.sh
 
-export PATH=$PATH:/usr/local/google-cloud-sdk/bin/
-echo "export $PATH"
-echo "gcloud init"
+echo "Log out of your current shell, or add the SDK to your PATH:"
+echo ""
+echo "    export PATH=\$PATH:\${HOME}/local/google-cloud-sdk/bin/"
+echo "Then run:"
+echo "    gcloud init"
